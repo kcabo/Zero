49fb7938e0785e60a73deda24d28f94792a1a713
Kcabo <37576764+kcabo@users.noreply.github.com>
2019-10-10 20:25:20 +0900

    ポートを固定

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
