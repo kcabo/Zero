72b7d9745191c5b4bf37d8f4fe375f2787d15b67
Kcabo <k7cabo@gmail.com>
2019-10-12 22:20:06 +0900

    rankingページを追加

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+
+from env import Record, ret_session
+
+ss = ret_session()
 
 app = Flask(__name__)
 
@@ -9,6 +13,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ranking')
+def ranking():
+    group = request.args.get('group')
+    style = request.args.get('style')
+    distance = request.args.get('distance')
+
+    records = ss.query(Record).all()
+
+    return render_template('ranking.html', records = records[:10])
+
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    if os.name == "nt": #ローカルの自機Windowsのとき
+        app.run(debug=True)
+    else:
+        port = int(os.getenv("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
