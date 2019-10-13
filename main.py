e4df45f684edd35a271484969760cead77968714
Kcabo <37576764+kcabo@users.noreply.github.com>
2019-10-13 13:30:06 +0900

    debug

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from flask import Flask
 
-# 自分自身の名前をappという変数でインスタンス化
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-# コマンドラインで本ファイルを起動させたときの動作
 if __name__ == '__main__':
-    # 安全のため debug=False とする
-    # 特に本番稼働するファイルでは debug=True としてはいけない!
-    app.run(debug=False)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
 # import datetime
 # import json
 # import os
