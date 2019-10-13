26cf2f4c0a63143ded8a4927a6d52fbec7c44f20
Kcabo <k7cabo@gmail.com>
2019-10-13 13:59:48 +0900

    logconfig

--- router.py
+++ router.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(0)
     return 'Hello World!'
 
 if __name__ == '__main__':
+    print('started')
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
