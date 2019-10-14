016d576d7cba8d2586d2f723e5fbd4f5229d7331
Kcabo <k7cabo@gmail.com>
2019-10-14 18:19:14 +0900

    moved new heroku enviroment

--- apps/env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def database_url():
-    if os.name == "nt": #ローカルの自機Windowsのとき
-        return 'postgresql://srp:0000@localhost:5432/srp'
-    else:
-        return os.environ['DATABASE_URL']
