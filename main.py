d1a99ab6bd98bfb33a737ed317370aff507a3f35
Kcabo <37576764+kcabo@users.noreply.github.com>
2019-10-18 14:28:11 +0900

    テスト用コメントアウトを削除

--- main.py
+++ main.py
@@ -5,13 +5,28 @@ import re
 import requests
 
 from flask import Flask, request, render_template
-# from env import database_url
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+if os.name == 'nt':
+    from env import database_url
+else:
+    database_url = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #これ書かないとログがうるさくなる
 
 # from models import db, Meet, Event, Record, RelayResult
+
+# ##test##
+# from models import create_table, Hoge
+#
+# create_table()
+#
+# # a1 = Hoge('a', '0:32.00')
+# # a2 = Hoge('a', '0:28.00')
+# # a3 = Hoge('a', '12:29.00')
+#
+# # db.session.add_all([a1, a2, a3])
+# # db.session.commit()
 
 
 @app.route('/')
@@ -24,10 +39,10 @@ def ranking():
     group = request.args.get('group')
     style = request.args.get('style')
     distance = request.args.get('distance')
+    records = list()
+    # records = db.session.query(Hoge).distinct(Hoge.name).all()#.order_by(Hoge.time).all()
 
-    # records = Record.query.all()
-
-    return render_template('ranking.html', records =[])
+    return render_template('ranking.html', records = records)
 
 if __name__ == "__main__":
     if os.name == "nt": #ローカルの自機Windowsのとき
---
+++ pptest.py
@@ -0,0 +1,6 @@
+import flask
+
+print('import完了')
+
+def sayhello():
+    print('hello')
