6abfb3d6544aa851e47c3513d2ce20a5b4915697
Kcabo <37576764+kcabo@users.noreply.github.com>
2019-11-09 14:35:19 +0900

    idによる選手検索に対応 ダッシュボードは見出しのみ対応

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
+import analyzer
 from constant import style_dic, distance_dic, area_list
 from format import del_space, del_numspace, format_time
 from task_manager import Takenoko, free, busy, get_status
@@ -129,10 +130,6 @@ class Record(db.Model): #個人種目の１記録
         self.time = format_time(del_space(self.time))
         self.laps = ",".join([format_time(del_space(lap)) for lap in self.laps])
 
-    def export_tupple(self):
-        return self.id, self.name, self.team, self.grade, self.time
-
-
 class Relay(db.Model): #リレーの１記録
     __tablename__ = 'relays'
     id = db.Column(db.Integer, primary_key=True)                      # 連番で振られるid
@@ -240,37 +237,32 @@ def index():
 def wake_up(): # 監視サービスで監視する用のURL
     return 'ok'
 
-@app.route('/dashboard')
-def dashboard():
 
-    # まずはレコードテーブルからIDを受け取りそこから「誰の」IDなのかを検索
-    id = request.args.get('id', 1, type=int)
+@app.route('/dashboard', methods = ['POST', 'GET'])
+def dashboard():
+    if request.method == 'GET':
+        id = request.args.get('id', 1, type=int)
+    else:
+        id = request.form.get('id', 1, type=int)
+
+    # まずは「誰の」IDなのかを検索
     swimmer = db.session.query(Record).get(id)
+    sex = swimmer.sex
+    name = swimmer.name
+    grade = swimmer.grade
 
     # 取得した選手の性別・名前・学年でフィルタリングしてrecordsテーブルから取得
-    # 上と同時にそれぞれのrecordのmeetidからMeetを内部結合
+    # 同時にそれぞれのrecordのmeetidからMeetを内部結合
     records = (db.session.query(Record, Meet)
-            .filter(Record.sex == swimmer.sex, Record.name == swimmer.name, Record.grade == swimmer.grade, Record.meetid == Meet.meetid)
+            .filter(Record.sex == sex, Record.name == name, Record.grade == grade, Record.meetid == Meet.meetid)
             .all())
 
     # 見出しの選手情報：     性別　名前　学年　所属(複数ある)
     teams = {r.Record.team for r in records}
-    print(teams)
+    info = {'sex':'men' if sex == 1 else 'women', 'name':name, 'grade':grade, 'teams':teams}
 
-    # 全レコード：          id, 種目(50Fr)　スタイル　距離　記録　水路(l,m,長,短)　日付　大会名 のデータフレームを作成
-    fixed = map(lambda x:(x.Record.id, x.Record.style, x.Record.distance, x.Record.time, x.Meet.pool, x.Meet.start, x.Meet.name), records)
-    df = pd.DataFrame(fixed, columns = ['id', 'style', 'distance', 'time', 'pool', 'start', 'meet_name'])
-    print(df)
-
-    # 出場回数：            全出場回数　五スタイルのそれぞれの回数(リストか辞書)　スタイルと距離を結合（種目）し、そのなかでの最多(S1)　偏差値(保留)
-    # 上の段階で折れ線グラフ化する2種目を選ぶ ←S1のスタイルのなかから、もっとも出場している種目を2つ選ぶ ←今後３つとかにしてもいいよね
-    # 調子折れ線グラフ：     2種目2水路に分ける。日付のシリアル化。記録の並び替え（1:経過日数少ない順,2:タイム早い順）して、日数が被ってるのを重複削除
-    #                      タイムを数値変換。最大値(最遅)からそれぞれの記録を引き算。その結果の中での最大値(最速)をdとし100/dをそれぞれに乗算
-    #                      {x:経過日数,y:計算結果}のリストを返す。それを4回繰り返す。
-    # データフレームを（早い順、日付最新順）に並び替え。種目で重複削除。
-    # 6カテゴリ(5種目＋長距離)の変数を作る。型はリストかディクショナリ。すべての抽出結果をその中に格納。（ベストのないカテゴリはFalseを返すようにする）
-
-    return render_template('dashboard.html')
+    analyzed = analyzer.swimmer_statisctics(records)
+    return render_template('dashboard.html', info=info)
 
 # TODO: リレーの記録も結合させる
 @app.route('/ranking')
@@ -285,11 +277,14 @@ def ranking():
             .limit(20000)
             .all()) # sortはORM側でやるのが早いのかそれともpandasに渡してからやったほうが早いのか…
     print(f'query records length:{len(records)}')
-    fixed = map(lambda x:x.Record.export_tupple(), records)
+
+    fixed = map(lambda x:(x.Record.id, x.Record.name, x.Record.team, x.Record.grade, x.Record.time), records)
     df = pd.DataFrame(fixed, columns = ['id', 'name', 'team', 'grade', 'time'])
     df.sort_values(['time'], inplace=True)
     df.drop_duplicates(subset=['name','grade'], inplace=True)
     return render_template('ranking.html', records = df[:100], pool=pool, sex=sex, style=style, distance=distance)
+
+
 
 @app.route(manegement_url) # commandなしのURLの場合、Noneが代入される
 @app.route(manegement_url + '/<command>')
